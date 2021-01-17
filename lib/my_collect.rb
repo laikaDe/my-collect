6f6411e8e7f86c31a28ea0c3f9c1208fb8bf27a6
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-01-16 18:23:41 -0800

    commit tree a289a5b6fe519a8beecab7636cd48ed0f8a13c5d

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+    collection =[]
+    if block_given?
+        int = 0
+        while int < array.length
+            collection << yield(array[int])
+            int += 1 
+        end
+        collection
+    else
+        print "No block given!"
+    end
+end
+
+my_collect(["Tim", "Tom", "Jim"]) do |name|
+    name.split(" ").first
+end
+
+
+
+
 
 
